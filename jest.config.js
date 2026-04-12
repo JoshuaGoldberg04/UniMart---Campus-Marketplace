413144429a756de98523034941cc3877e799f5a9
JoshuaGoldberg04 <2860656@students.wits.ac.za>
2026-04-12 21:15:12 +0200

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: "node",
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov"],
-};
