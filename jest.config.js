d46afa08466bf5ee147720595bfada3ff665abde
JoshuaGoldberg04 <2860656@students.wits.ac.za>
2026-05-10 22:49:20 +0200

    Add files via upload

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,28 @@
-module.exports = {
-  testEnvironment: "node",
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov"],
+export default {
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
   collectCoverageFrom: [
-    "*.js",
-    "!jest.config.js"
-  ]
+    'frontend/**/*.{js,jsx}',
+    '!frontend/**/*.test.{js,jsx}',
+    '!frontend/**/__tests__/**',
+    '!**/node_modules/**',
+    '!**/vendor/**'
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/tests/',
+    'jest.config.js'
+  ],
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleFileExtensions: ['js', 'json'],
+  transform: {},
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/frontend/$1',
+    '^@scripts/(.*)$': '<rootDir>/frontend/scripts/$1',
+    '^@pages/(.*)$': '<rootDir>/frontend/pages/$1',
+    '^@styles/(.*)$': '<rootDir>/frontend/styles/$1'
+  },
+  testTimeout: 10000
 };
